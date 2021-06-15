fd45c1a1ab5674cc919a4e684995c66d9efb0156
theendofunity <dwd96@yandex.ru>
2021-06-15 23:08:53 +0700

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Дмитрий Дудкин on 15.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
