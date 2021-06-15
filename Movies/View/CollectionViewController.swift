af87117e2d4ddc1777ac5906b146b312fadbe522
theendofunity <dwd96@yandex.ru>
2021-06-16 00:15:25 +0700

    basic cell ui

--- Movies/View/CollectionViewController.swift
+++ Movies/View/CollectionViewController.swift
@@ -8,11 +8,11 @@
 import UIKit
 
 class CollectionViewController: UICollectionViewController {
-//    MARK: - Properties
+    //    MARK: - Properties
     
     let viewModel: MoviesViewModelType
     
-//    MARK: - Initializers
+    //    MARK: - Initializers
     
     init(viewModel: MoviesViewModelType) {
         self.viewModel = viewModel
@@ -24,8 +24,8 @@ class CollectionViewController: UICollectionViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
- 
-//    MARK: Life cycle
+    
+    //    MARK: Life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,7 +42,6 @@ class CollectionViewController: UICollectionViewController {
         return 1
     }
     
-    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return viewModel.numberOfItems()
     }
@@ -55,37 +54,6 @@ class CollectionViewController: UICollectionViewController {
         
         return cell
     }
-    
-    // MARK: UICollectionViewDelegate
-    
-    /*
-     // Uncomment this method to specify if the specified item should be highlighted during tracking
-     override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-     return true
-     }
-     */
-    
-    /*
-     // Uncomment this method to specify if the specified item should be selected
-     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-     return true
-     }
-     */
-    
-    /*
-     // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-     override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-     return false
-     }
-     
-     override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-     return false
-     }
-     
-     override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-     
-     }
-     */
     
     // MARK: - UI Configuration
     
--- Movies/View/MovieCollectionViewCell.swift
+++ Movies/View/MovieCollectionViewCell.swift
@@ -9,8 +9,39 @@ import UIKit
 
 class MovieCollectionViewCell: UICollectionViewCell {
 //    MARK: - Properties
+    
     static let identifier = "MovieCollectionViewCell"
-    var viewModel: MovieCellViewModelType?
+    let poster = UIImageView()
+
+    var viewModel: MovieCellViewModelType? {
+        willSet {
+            guard let newPoster = viewModel?.moviePoster else { return }
+            poster.image = newPoster
+        }
+    }
     
+//    MARK: - Initializers
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+//    MARK: - UISetup
+    
+    private func setupLayout() {
+        poster.image = UIImage(named: "moviePlaceholder")
+        poster.translatesAutoresizingMaskIntoConstraints = false
+
+        addSubview(poster)
+        
+        poster.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        poster.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        poster.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        poster.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+    }
 }
